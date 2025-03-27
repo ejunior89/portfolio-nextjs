799ba28ea40e4e83d62d7343e3d23c9266230d84
ejunior89 <eliomaiajunior@gmail.com>
2025-03-27 15:15:09 -0400

    corrigindo caminhos

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,7 +10,7 @@ import Footer from '../components/Footer';
 
 const Index = () => {
   return (
-    <BrowserRouter basename="/portfolio-nextjs">
+    <BrowserRouter basename="">
       <div className="min-h-screen bg-white dark:bg-gray-900">
         <Header />
         <main>
