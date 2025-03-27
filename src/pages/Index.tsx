724536db6432276e4b6084ff90b5e6ea5455f729
ejunior89 <eliomaiajunior@gmail.com>
2025-03-27 15:21:00 -0400

    corrigindo caminhos

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,7 +10,7 @@ import Footer from '../components/Footer';
 
 const Index = () => {
   return (
-    <BrowserRouter basename="">
+    <BrowserRouter>
       <div className="min-h-screen bg-white dark:bg-gray-900">
         <Header />
         <main>
